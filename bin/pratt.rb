406d80ebaf3ea8a205cf7b1b23aa88e2d703df05
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-03-09 02:44:14 -0700

    Moved around cli arguments. Removed extra temps. Removed 'faux' duplicate code. Fixed daemonization.

--- bin/pratt.rb
+++ bin/pratt.rb
@@ -12,61 +12,63 @@ opts.prompt       = nil
 opts.graph        = false
 
 ARGV.options do |opt|
-  opt.on('-i', "--interval INTERVAL", Float, "List of projects to display.") do |intvl|
-    opts.interval = intvl
+  opt.on('-b', "--begin PROJECT_NAME", String, "Begin project tracking.") do |proj|
+    Project.find_or_create_by_name( { :name => proj }).start!
   end
-  opt.on('-r PROJECT_NAME', "--restart PROJECT_NAME", String, "Restart project (stop last log and start a new one).") do |proj|
-    Pratt.new.do :restart!, proj
+  opt.on('-r', "--restart PROJECT_NAME", String, "Restart project (stop last log and start a new one).") do |proj|
+    Project.find_or_create_by_name( { :name => proj }).restart!
   end
-  opt.on('-b PROJECT_NAME', "--begin PROJECT_NAME", String, "Begin project tracking.") do |proj|
-    Pratt.new.do :start!, proj
+  opt.on('-e', "--end PROJECT_NAME", String, "End project tracking.") do |proj|
+    Project.find_or_create_by_name( { :name => proj }).stop!
   end
-  opt.on('-e PROJECT_NAME', "--end PROJECT_NAME", String, "End project tracking.") do |proj|
-    Pratt.new.do :stop!, proj
+  opt.on('-c', "--change PROJECT_NAME", String, "End project tracking.") do |proj|
+    last_log = Whence.last
+    last_log.project = Project.find_or_create_by_name proj
+    last_log.save
   end
-  opt.on('-c PROJECT_NAME', "--change PROJECT_NAME", String, "End project tracking.") do |proj|
-    Pratt.new.change proj
-  end
+
   opt.on('-g', "--graph [PROJECT_NAME]", String, "End project tracking.") do |proj|
     if proj
-      puts "#{(proj = Project.find_by_name(proj)).name}: #{proj.time_spent}"
+      puts "#{(proj = Project.find_by_name(proj)).name}: #{proj.time_spent(true)}"
     else
       projects = Project.all
-      max      = projects.inject(0) do |x,p| 
+      max = projects.inject(0) do |x,p| 
         x = p.name.length if p.name.length > x
         x
       end
-      puts projects.collect {|proj| "%1$*3$s: %2$s"% [proj.name, proj.time_spent, max] }
+      puts projects.collect {|proj| "%1$*3$s: %2$s"% [proj.name, proj.time_spent(true), max] }
     end
   end
-
   opt.on('-s', "--show", String, "Show available projects and current project (if there is one)") do |proj|
     projects = ([Project.refactor, Project.off] | Project.rest).collect(&:name)
-    puts "projects: '#{projects*"','"}'"
-    if (current = Whence.last).end_at.nil?
-      puts " current: '#{current.project.name}'" 
-      puts " started: '#{current.start_at}'"
-    else
-      puts " current: No current project" 
-    end
+    current = Whence.last
+
+    puts "projects: " << projects.collect {|project| "'#{project.send(current.end_at.nil? && current.project.name == project ? :green : :magenta)}'" }*', '
+    puts " started: '#{current.start_at}'" if current.end_at.nil?
+  end
+
+  opt.on('-i', "--interval INTERVAL", Float, "List of projects to display.") do |interval|
+    opts.interval = interval
   end
   opt.on("-d", "--daemonize", "Start daemon.") do
     opts.do_daemonize = true
   end
-  opt.on("--and_quit", "Stop daemon.") do
-    opts.quit = false
-  end
   opt.on('-q', "--quit", "Stop daemon.") do
-    opts.quit = false
+    opts.quit = true
   end
-  opt.on('-p WHICH_GUI', '--prompt WHICH_GUI', [:main, :pop], "Flag for prompting a gui (currently: main or pop).") do |gui|
-    opts.prompt = gui
+  opt.on('-p', '--prompt GUI', [:main, :pop], "Flag for prompting a gui (currently: main or pop).") do |gui|
+    Pratt.send gui
   end
   
   opt.parse!
 end
 
-opts.interval = opts.interval.to_f
+if opts.do_daemonize 
+  if Pratt.daemonized?
+    puts "Pratt appears to be still running at pid (#{File.open(Pratt::PID_FILE).readline.yellow})."
+  else
+    Pratt.run(opts.interval)
+  end
+end
 
-Pratt.run(opts.interval) if opts.do_daemonize
-Pratt.new.send opts.prompt if opts.prompt
+Pratt.quit if opts.quit
