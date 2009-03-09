f29a88ab41301227865d217984f83e99d02db5c7
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-03-09 02:46:31 -0700

    Made instance methods class methods since they weren't being used as instance methods

--- lib/pratt.rb
+++ lib/pratt.rb
@@ -7,64 +7,51 @@ class Pratt
   VERSION = '1.0.0'
   PID_FILE='pratt.pid'
 
-  def initialize
-  end
-
-  def main
-    projects = ([Project.refactor, Project.off] | Project.rest).collect(&:name)
-    system("ruby lib/main.rb --projects '#{projects*"','"}' --current '#{Whence.last.project.name}'")
-  end
-  def pop
-    project = Whence.last.project
-    system("ruby lib/pop.rb '#{project.name}' '#{project.time_spent}'")
-  end
-  def do action, project
-    Project.find_by_name(project).send action
-  end
-  def change project_name
-    last_log = Whence.last
-    last_log.project = Project.find_or_create_by_name project_name
-    last_log.save
-  end
-
-  def quit
-    Process.kill("KILL", File.open(PID_FILE).readline.to_i) and self.class.rm_pid!
-  end
-
   class << self
-    def interval
-      @@interval || 15*60
-    end
-    def interval= i
-      @@interval = i*60
+    def quit
+      Process.kill("KILL", File.open(PID_FILE).readline.to_i) && rm(PID_FILE)
     end
 
-    def run intvl = 15, daemonize = false
-      self.interval = intvl
-      (me = new).main
-      puts DateTime.now.strftime
-      daemonize!
-      sleep(interval)
-      while(daemonized?)
-        me.pop
-        sleep(interval)
-      end
-      rm_pid!
+    def main
+      projects = ([Project.refactor, Project.off] | Project.rest).collect(&:name)
+      Process.detach(
+        fork { system("ruby lib/main.rb --projects '#{projects*"','"}' --current '#{Whence.last.project.name}'") } 
+      )
+    end
+
+    def pop
+      project = Whence.last.project
+      Process.detach(
+        fork { system("ruby lib/pop.rb '#{project.name}' '#{project.time_spent}'") } 
+      )
+    end
+
+    def run interval = 15.0
+      Process.detach(
+        fork {
+          daemonize!
+          (me = new).main
+          while(daemonized?)
+            sleep(interval*60)
+            me.pop
+          end
+        }
+      )
     end
 
     def daemonized?
-      File.exists?(PID_FILE)
+      File.exists?(PID_FILE) and File.open(PID_FILE).readline.to_i != 0
     end
-    
-    def rm_pid!
-      rm PID_FILE
-    end
+
     private
+      def pid
+        File.open(PID_FILE).readline.to_i
+      end
+
       def daemonize!
-        puts "pratt (#{Process.pid})"
-        File.open PID_FILE, 'w' do |f|
-          f.write(Process.pid)
-        end
+#        return if daemonized?
+        puts "pratt (#{Process.pid.to_s.yellow})"
+        File.open(PID_FILE, 'w') {|f| f.write(Process.pid) }
       end
   end
 end
