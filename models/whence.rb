9f12afcc4b6a71b1f5b70da25f415d1a86e055bd
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-03-10 09:20:04 -0700

    Added format, stop!, and fixed validation when end_at is nil

--- models/whence.rb
+++ models/whence.rb
@@ -1,7 +1,26 @@
 class Whence < ActiveRecord::Base
   belongs_to :project
 
+  def stop!
+    end_at ||= DateTime.now
+    save
+  end
+
+  def start_at
+    sa = read_attribute(:start_at)
+    class << sa
+      def to_s
+        self.strftime("%a %d %b %Y %X")
+      end
+    end
+    sa
+  end
+
   class << self
+    def last_unended
+      first :conditions => "end_at IS NULL"
+    end
+
     def migrate up = true 
       ActiveRecord::Schema.define do
         if up
@@ -19,6 +38,8 @@ class Whence < ActiveRecord::Base
 
   protected
     def validate
+      if end_at
         errors.add "time", "end time must be greater than start time" unless end_at > start_at
+      end
     end
 end
