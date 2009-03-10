eeee151222ececa07877fbfd54eca3c54daed395
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-03-10 09:22:33 -0700

    adding time argument to program options

--- bin/pratt.rb
+++ bin/pratt.rb
@@ -11,7 +11,8 @@ opts.quit         = false
 opts.do_daemonize = false
 opts.prompt       = nil
 opts.graph        = false
-opts.whence       = Time.now
+opts.week         = nil
+opts.day          = nil
 
 ARGV.options do |opt|
   opt.on('-b', "--begin PROJECT_NAME", String, "Begin project tracking.") do |proj|
@@ -24,30 +25,42 @@ ARGV.options do |opt|
     Project.find_or_create_by_name( { :name => proj }).stop!
   end
   opt.on('-c', "--change PROJECT_NAME", String, "End project tracking.") do |proj|
-    last_log = Whence.last
+    last_log = Whence.last_unended
     last_log.project = Project.find_or_create_by_name proj
     last_log.save
   end
 
-  opt.on('-w', '--when TIME', Time, "What time do you log this") do |whence|
-    opts.whence = whence
+  opt.on('-w', '--week TIME', Time, "What week do you want graphed?") do |week|
+    opts.week = week
+  end
+  opt.on('-a', '--day TIME', Time, "What week do you want graphed?") do |day|
+    opts.day = day
   end
 
   opt.on('-g', "--graph [PROJECT_NAME]", String, "End project tracking.") do |proj|
-    if proj
-      puts "#{(proj = Project.find_by_name(proj)).name}: #{proj.time_spent(true)}"
-    else
-      projects = Project.all
-      max = projects.inject(0) do |x,p| 
-        x = p.name.length if p.name.length > x
-        x
+    if opts.week.nil?
+      if proj
+        projects = [Project.find_by_name(proj)]
+        max      = projects.first.name.size
+      else
+        projects = Project.all
+        max = projects.inject(0) {|x,p| x = p.name.length if p.name.length > x; x }
       end
-      puts projects.collect {|proj| "%1$*3$s: %2$s"% [proj.name, proj.time_spent(true), max] }
+    else
+      if proj
+        projects = [Project.find_by_name(proj).whences.all(:conditions => ["start_at BETWEEN ? AND ?", opts.week.beginning_of_week])]
+        max      = projects.first.name.size
+      else
+        projects = Project.all :include => :whences, :conditions => ["start_at BETWEEN ? AND ?", opts.week.beginning_of_week]
+        max = projects.inject(0) {|x,p| x = p.name.length if p.name.length > x; x }
+      end
     end
+    puts projects.collect {|proj| "%1$*3$s: %2$s"% [proj.name, proj.time_spent(true), max] }
+
   end
   opt.on('-s', "--show", String, "Show available projects and current project (if there is one)") do |proj|
     projects = ([Project.refactor, Project.off] | Project.rest).collect(&:name)
-    current = Whence.last
+    current = Whence.last_unended
 
     puts "projects: " << projects.collect {|project| "'#{project.send(current.end_at.nil? && current.project.name == project ? :green : :magenta)}'" }*', '
     puts " started: '#{current.start_at}'" if current.end_at.nil?
@@ -60,6 +73,7 @@ ARGV.options do |opt|
     opts.do_daemonize = true
   end
   opt.on('-q', "--quit", "Stop daemon.") do
+    Whence.last_unended.stop!
     opts.quit = true
   end
   opt.on('-p', '--prompt GUI', [:main, :pop], "Flag for prompting a gui (currently: main or pop).") do |gui|
