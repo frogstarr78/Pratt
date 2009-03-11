5edd5e666d42ca3b9621fb7ac66febe4dabbc1f8
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-03-11 08:30:06 -0700

    Moved argument processing into library

--- bin/pratt.rb
+++ bin/pratt.rb
@@ -4,91 +4,96 @@ require 'optparse'
 require 'optparse/time'
 require 'ostruct'
 
-opts = OpenStruct.new
-opts.show         = false
-opts.interval     = 15
-opts.quit         = false
-opts.do_daemonize = false
-opts.prompt       = nil
-opts.graph        = false
-opts.week         = nil
-opts.day          = nil
+#opts = OpenStruct.new
+#opts.interval     = 15
+#opts.quit         = false
+#opts.do_daemonize = false
+#opts.prompt       = nil
+#opts.show         = false
+#opts.graph        = false
+#opts.week         = false
+#opts.day          = false
+#opts.when_to      = nil
 
-ARGV.options do |opt|
-  opt.on('-b', "--begin PROJECT_NAME", String, "Begin project tracking.") do |proj|
-    Project.find_or_create_by_name( { :name => proj }).start!
-  end
-  opt.on('-r', "--restart PROJECT_NAME", String, "Restart project (stop last log and start a new one).") do |proj|
-    Project.find_or_create_by_name( { :name => proj }).restart!
-  end
-  opt.on('-e', "--end PROJECT_NAME", String, "End project tracking.") do |proj|
-    Project.find_or_create_by_name( { :name => proj }).stop!
-  end
-  opt.on('-c', "--change PROJECT_NAME", String, "End project tracking.") do |proj|
-    last_log = Whence.last_unended
-    last_log.project = Project.find_or_create_by_name proj
-    last_log.save
-  end
+#ARGV.options do |opt|
+#  opt.on('-b', "--begin PROJECT_NAME", String, "Begin project tracking.") do |proj|
+#    Project.find_or_create_by_name( { :name => proj }).start!
+#  end
+#  opt.on('-r', "--restart PROJECT_NAME", String, "Restart project (stop last log and start a new one).") do |proj|
+#    Project.find_or_create_by_name( { :name => proj }).restart!
+#  end
+#  opt.on('-e', "--end PROJECT_NAME", String, "End project tracking.") do |proj|
+#    Project.find_or_create_by_name( { :name => proj }).stop!
+#  end
+#  opt.on('-c', "--change PROJECT_NAME", String, "End project tracking.") do |proj|
+#    last_log = Whence.last_unended
+#    last_log.project = Project.find_or_create_by_name proj
+#    last_log.save
+#  end
+#
+#  opt.on '-w', '--when_to', Time, 'When to do something.' do |when_to|
+#    opts.when_to = when_to
+#  end
+#  opt.on('-W', '--Week', "Time argument applies to a week. (Only for graphing)") do |week|
+#    opts.week = true
+#  end
+#  opt.on('-D', '--Day', "Time argument applies to a day.") do |day|
+#    opts.day = true
+#  end
+#
+#  opt.on('-g', "--graph [PROJECT_NAME]", String, "End project tracking.") do |proj|
+#    if opts.week.nil?
+#      if proj
+#        projects = [Project.find_by_name(proj)]
+#        max      = projects.first.name.size
+#      else
+#        projects = Project.all
+#        max = projects.inject(0) {|x,p| x = p.name.length if p.name.length > x; x }
+#      end
+#    else
+#      if proj
+#        projects = [Project.find_by_name(proj).whences.all(:conditions => ["start_at BETWEEN ? AND ?", opts.week.beginning_of_week])]
+#        max      = projects.first.name.size
+#      else
+#        projects = Project.all :include => :whences, :conditions => ["start_at BETWEEN ? AND ?", opts.week.beginning_of_week]
+#        max = projects.inject(0) {|x,p| x = p.name.length if p.name.length > x; x }
+#      end
+#    end
+#    puts projects.collect {|proj| "%1$*3$s: %2$s"% [proj.name, proj.time_spent(true), max] }
+#
+#  end
+#  opt.on('-s', "--show", String, "Show available projects and current project (if there is one)") do |proj|
+#    projects = ([Project.refactor, Project.off] | Project.rest).collect(&:name)
+#    current = Whence.last_unended || Whence.last
+#
+#    puts "projects: " << projects.collect {|project| "'#{project.send(current.end_at.nil? && current.project.name == project ? :green : :magenta)}'" }*', '
+#    puts " started: '#{current.start_at}'" if current.end_at.nil?
+#  end
+#
+#  opt.on('-i', "--interval INTERVAL", Float, "List of projects to display.") do |interval|
+#    opts.interval = interval
+#  end
+#  opt.on("-d", "--daemonize", "Start daemon.") do
+#    opts.do_daemonize = true
+#  end
+#  opt.on('-q', "--quit", "Stop daemon.") do
+#    Whence.last_unended.stop!
+#    opts.quit = true
+#  end
+#  opt.on('-p', '--prompt GUI', [:main, :pop], "Flag for prompting a gui (currently: main or pop).") do |gui|
+#    Pratt.send gui
+#  end
+#  
+#  opt.parse!
+#end
+Pratt.parse ARGV
 
-  opt.on('-w', '--week TIME', Time, "What week do you want graphed?") do |week|
-    opts.week = week
-  end
-  opt.on('-a', '--day TIME', Time, "What week do you want graphed?") do |day|
-    opts.day = day
-  end
-
-  opt.on('-g', "--graph [PROJECT_NAME]", String, "End project tracking.") do |proj|
-    if opts.week.nil?
-      if proj
-        projects = [Project.find_by_name(proj)]
-        max      = projects.first.name.size
-      else
-        projects = Project.all
-        max = projects.inject(0) {|x,p| x = p.name.length if p.name.length > x; x }
-      end
-    else
-      if proj
-        projects = [Project.find_by_name(proj).whences.all(:conditions => ["start_at BETWEEN ? AND ?", opts.week.beginning_of_week])]
-        max      = projects.first.name.size
-      else
-        projects = Project.all :include => :whences, :conditions => ["start_at BETWEEN ? AND ?", opts.week.beginning_of_week]
-        max = projects.inject(0) {|x,p| x = p.name.length if p.name.length > x; x }
-      end
-    end
-    puts projects.collect {|proj| "%1$*3$s: %2$s"% [proj.name, proj.time_spent(true), max] }
-
-  end
-  opt.on('-s', "--show", String, "Show available projects and current project (if there is one)") do |proj|
-    projects = ([Project.refactor, Project.off] | Project.rest).collect(&:name)
-    current = Whence.last_unended
-
-    puts "projects: " << projects.collect {|project| "'#{project.send(current.end_at.nil? && current.project.name == project ? :green : :magenta)}'" }*', '
-    puts " started: '#{current.start_at}'" if current.end_at.nil?
-  end
-
-  opt.on('-i', "--interval INTERVAL", Float, "List of projects to display.") do |interval|
-    opts.interval = interval
-  end
-  opt.on("-d", "--daemonize", "Start daemon.") do
-    opts.do_daemonize = true
-  end
-  opt.on('-q', "--quit", "Stop daemon.") do
-    Whence.last_unended.stop!
-    opts.quit = true
-  end
-  opt.on('-p', '--prompt GUI', [:main, :pop], "Flag for prompting a gui (currently: main or pop).") do |gui|
-    Pratt.send gui
-  end
-  
-  opt.parse!
-end
-
-if opts.do_daemonize 
-  if Pratt.daemonized?
-    puts "Pratt appears to be still running at pid (#{File.open(Pratt::PID_FILE).readline.yellow})."
-  else
-    Pratt.run(opts.interval)
-  end
-end
-
-Pratt.quit if opts.quit
+#if opts.do_daemonize 
+#  if Pratt.daemonized?
+#    puts "Pratt appears to be still running at pid (#{File.open(Pratt::PID_FILE).readline.yellow})."
+#  else
+#    Pratt.run(opts.interval)
+#  end
+#end
+#
+#Pratt.quit if opts.quit
--- lib/pratt.rb
+++ lib/pratt.rb
@@ -8,9 +8,146 @@ class Pratt
 #  PID_FILE='/var/run/pratt.pid'
   PID_FILE='pratt.pid'
 
+  attr_accessor :interval, :quit, :do_daemonize, :prompt, :show, :week, :day, :when_to, :scale
+  attr_reader :graph, :project
+  def initialize proj = nil #interval, quit, do_daemonize, prompt, show, graph, week, day, when_to
+    @when_to = Time.now
+    @week    = false
+    @day     = false
+    @do      = []
+    @scale   = nil
+    self.project = proj
+#    @interval, @quit, @do_daemonize, @prompt, @show, @graph, @week, @day, @when_to = 
+  end
+
+  def project= proj
+    @project = Project.find_or_create_by_name( { :name => proj } )
+  end
+
+  def << what
+    @do << what
+  end
+
+  def graph
+    projects = 
+    if project?
+      [project]
+    else
+      Project.all
+    end
+    max = projects.inject(0) {|x,p| x = p.name.length if p.name.length > x; x }
+    puts projects.collect {|proj| "%1$*3$s: %2$s"% [proj.name, proj.time_spent(true, scale, when_to), max] }
+  end
+
+  def show
+    project_names = ([Project.refactor, Project.off] | Project.rest).collect(&:name)
+    current = Whence.last_unended || Whence.last
+
+    puts "projects: " << project_names.collect {|project_name| "'#{project_name.send(current.end_at.nil? && current.project.name == project_name ? :green : :magenta)}'" }*', '
+    puts " started: '#{current.start_at}'" if current.end_at.nil?
+  end
+
+  def begin
+    project.start!
+  end
+  def restart
+    project.restart!
+  end
+  def end
+    project.stop!
+  end
+  def change
+    Whence.last_unended.change! project.name
+  end
+
+  def run
+    self.begin      if i_should?(:begin)
+    self.restart    if i_should?(:restart)
+    self.end        if i_should?(:end)
+    self.change     if i_should?(:change)
+    self.graph      if i_should?(:graph)
+    self.show       if i_should?(:show)
+    self.class.run  if i_should?(:do_daemonize)
+    self.quit       if i_should?(:quit)
+  end
+
+  def quit
+    Whence.last_unended.stop!
+    Process.kill("KILL", File.open(PID_FILE).readline.to_i) && rm(PID_FILE)
+  end
+
+  private
+    def i_should? what
+      @do.include?(what)
+    end
+
+    def project?
+      !@project.nil? and @project.name?
+    end
+
   class << self
-    def quit
-      Process.kill("KILL", File.open(PID_FILE).readline.to_i) && rm(PID_FILE)
+    def parse args
+      me = Pratt.new
+
+      args.options do |opt|
+#        opt.on('-P', "--Project PROJECT_NAME", String, "Just set the project and be done with it (aka other PROJECT_NAME argument values become optional).") do |proj|
+#          me.project = proj
+#          me << :begin
+#        end
+        opt.on('-b', "--begin PROJECT_NAME", String, "Begin project tracking.") do |proj|
+          me.project = proj
+          me << :begin
+        end
+        opt.on('-r', "--restart PROJECT_NAME", String, "Restart project (stop last log and start a new one).") do |proj|
+          me.project = proj
+          me << :restart
+          Project.find_or_create_by_name( { :name => proj }).restart!
+        end
+        opt.on('-e', "--end PROJECT_NAME", String, "End project tracking.") do |proj|
+          me.project = proj
+          me << :end
+          Project.find_or_create_by_name( { :name => proj }).stop!
+        end
+        opt.on('-c', "--change PROJECT_NAME", String, "End project tracking.") do |proj|
+          me.project = proj
+          me << :change
+        end
+        opt.on('-g', "--graph [PROJECT_NAME]", String, "End project tracking.") do |proj|
+          me.project = proj
+          me << :graph
+        end
+
+        opt.on '-w', '--when_to TIME', Time, 'When to do something.' do |when_to|
+          me.when_to = when_to
+        end
+        opt.on('-l', '--scale SCALE', [:day, :week, :month, :quarter, :year], "Granularity of time argument. (Only for graphing)") do |scale|
+          me.scale = scale
+        end
+        opt.on('-R', '--Raw', "Dump logs raw") do
+          puts Whence.all.collect(&:inspect)
+        end
+
+        opt.on('-s', "--show", "Show available projects and current project (if there is one)") do
+          me << :show
+        end
+
+        opt.on('-i', "--interval INTERVAL", Float, "List of projects to display.") do |interval|
+          me.interval = interval
+        end
+        opt.on("-d", "--daemonize", "Start daemon.") do
+          me << :do_daemonize
+        end
+        opt.on('-q', "--quit", "Stop daemon.") do
+          me << :quit
+        end
+        opt.on('-p', '--prompt GUI', [:main, :pop], "Flag for prompting a gui (currently: main or pop).") do |gui|
+          send gui
+        end
+        
+        opt.parse!
+      end
+
+      me.run
     end
 
     def main
