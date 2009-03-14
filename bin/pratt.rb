e8a73ce339d47def6a4390ca4dcd5b228801d02a
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-03-14 00:39:29 -0700

    Allowing to be run in any dir.

--- bin/pratt.rb
+++ bin/pratt.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/ruby
+
+require 'fileutils'
+FileUtils.chdir File.dirname(File.expand_path('..', File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__ ) )
 require "lib/pratt"
 
 Pratt.parse ARGV
