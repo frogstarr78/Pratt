9234067eac8b2153c3f5b3aab4a118da4c551598
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-03-14 23:35:17 -0700

    Moved into ARGV processing

--- bin/pratt.rb
+++ bin/pratt.rb
@@ -4,7 +4,4 @@ require 'fileutils'
 FileUtils.chdir File.dirname(File.expand_path('..', File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__ ) )
 require "lib/pratt"
 
-$stderr.reopen("log/pratt.log", 'w')
-$stderr.sync = true
-
 Pratt.parse(ARGV)
