379ae7c93712a731b26f76c2a8681b1c5829356a
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-04 01:33:47 -0700

    Fixed schema definition

--- models/customer.rb
+++ models/customer.rb
@@ -1,11 +1,15 @@
 class Customer < ActiveRecord::Base
+  has_many :projects
 
   class << self
     def migrate up = :up
       ActiveRecord::Schema.define do
-        create_table customers do |t|
+        create_table :customers do |t|
+          t.string :name, :null => false
+          t.string :address
+          t.string :zip, :limit => 5
         end if up == :up
-        drop_table customers if up == :down
+        drop_table :customers if up == :down
       end
     end
   end
