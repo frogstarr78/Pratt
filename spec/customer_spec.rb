3f6f4be4c634b7f46e0347113ae0dd510d491b52
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-04 15:28:03 -0700

    Adding scaffolded tests for customers and payments

---
+++ spec/customer_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'pratt'
+
+describe Customer do
+  it "has a test" do
+    flunk
+  end
+end
---
+++ spec/payment_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'pratt'
+
+describe Payment do
+  it "has a test" do
+    false.should be_true
+  end
+end
