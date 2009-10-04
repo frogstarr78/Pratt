ba6242bf78839df5ed4996baeff77e56eb3df736
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-03 23:32:00 -0700

    Cleaning this up a bit

--- config.rb
+++ config.rb
@@ -2,23 +2,11 @@ require 'active_record'
 
 class Pratt
   include FileUtils
-  DBFILES = (
-    Dir.glob("*.sqlite3").collect {|file| file.split('.').first }
-  )
 
   class << self
     def connect to_env = :development
-      ActiveRecord::Base.establish_connection(
-        :adapter => 'sqlite3',
-        :database  => case to_env
-        when :production, 'production'
-          'production.sqlite3'
-        when :test, 'test'
-          'test.sqlite3'
-        else
-          "#{to_env}.sqlite3"
-        end
-      )
+      puts "Connecting to: #{to_env}"
+      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database  => "#{to_env}.sqlite3" )
     end
 
     def connected?
@@ -29,5 +17,6 @@ class Pratt
 end
 
 #include Pratt::Config
-Pratt.connect :production
+#Pratt.connect :production
+Pratt.connect :development
 #migrate
