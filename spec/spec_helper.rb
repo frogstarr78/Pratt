fc7d3fb38dd463f312ed7052deef2ba3bbdee6d2
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-04 15:25:33 -0700

    Renamed tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,27 +12,27 @@ Spec::Runner.configure do |config|
 end
 
 shared_examples_for "Time spent on a project" do
-  it "should correctly calculate time_spent with time argument" do
+  it "correctly calculates time_spent with time argument" do
     when_to = Chronic.parse('monday this week')
     @project.start! when_to
     @project.stop!  when_to+3
     @project.time_spent.should == 3.0/3600
   end
 
-  it "should correctly calculate time_spent with string time argument" do
+  it "correctly calculates time_spent with string time argument" do
     @project.start! 'last monday 12:00 pm'
     @project.stop!  'last monday 12:00:05 pm'
     @project.time_spent.should == 5.0/3600
   end
 
-  it "should correctly calculate time_spent with a scale" do
+  it "correctly calculates time_spent with a scale" do
     whence = Chronic.parse('yesterday 11:53 pm')
     @project.start! whence
     @project.stop!  whence+17
     @project.time_spent('week').should == 17.0/3600
   end
 
-  it "should correctly calculate time_spent with a scale and specific time" do
+  it "correctly calculates time_spent with a scale and specific time" do
     whence = Chronic.parse('yesterday 11:54 pm')
     @project.start! whence
     @project.stop!  whence+18
