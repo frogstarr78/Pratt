84b09edefed79b7f36da299f58d6db62b8f2c02e
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-04 02:38:31 -0700

    Adding customer migration

--- migrations/add_customer_id_to_project.rb
+++ migrations/add_customer_id_to_project.rb
@@ -2,5 +2,5 @@ add_column 'projects', :customer_id, :integer
 Customer.migrate :up
 qs = Customer.create :name => "QualitySmith LLC", :address => "106 N 2nd Street", :zip => "99362"
 Project.update_all "customer_id = '#{qs.id}'"
-Project.off.update_attribute :customer_id => 0
+Project.off.update_attribute :customer_id, 0
 change_column_null 'projects', :customer_id, false
