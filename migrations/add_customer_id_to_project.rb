e7fae893fad7669ac2b10c40062098aacfe47902
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-05 14:12:24 -0700

    Removing sensative data

--- migrations/add_customer_id_to_project.rb
+++
@@ -1,6 +0,0 @@
-add_column 'projects', :customer_id, :integer
-Customer.migrate :up
-qs = Customer.create :name => "QualitySmith LLC", :address => "106 N 2nd Street", :zip => "99362"
-Project.update_all "customer_id = '#{qs.id}'"
-Project.off.update_attribute :customer_id, 0
-change_column_null 'projects', :customer_id, false
