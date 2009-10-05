d09ad46cd93bd6d207d83c2e86d1f26695f8327f
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-05 09:36:39 -0700

    Removed shared behavior test that shouldn't be shared

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,32 +12,6 @@ Spec::Runner.configure do |config|
 end
 
 shared_examples_for "Time spent on a project" do
-  it "correctly calculates time_spent with time argument" do
-    when_to = Chronic.parse('monday this week')
-    @project.start! when_to
-    @project.stop!  when_to+3
-    @project.time_spent.should == 3.0/3600
-  end
-
-  it "correctly calculates time_spent with string time argument" do
-    @project.start! 'last monday 12:00 pm'
-    @project.stop!  'last monday 12:00:05 pm'
-    @project.time_spent.should == 5.0/3600
-  end
-
-  it "correctly calculates time_spent with a scale" do
-    whence = Chronic.parse('yesterday 11:53 pm')
-    @project.start! whence
-    @project.stop!  whence+17
-    @project.time_spent('week').should == 17.0/3600
-  end
-
-  it "correctly calculates time_spent with a scale and specific time" do
-    whence = Chronic.parse('yesterday 11:54 pm')
-    @project.start! whence
-    @project.stop!  whence+18
-    @project.time_spent('day', Chronic.parse('yesterday')).should == 18.0/3600
-  end
 end
 
 shared_examples_for "needing project instance that knows how to cleanup" do 
