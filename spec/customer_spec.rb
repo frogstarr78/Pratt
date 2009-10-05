1fa49548ac8285ab79dc4b23b8b0c99e82ce4fec
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-04 21:02:33 -0700

    Added testing for amount

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,17 +2,30 @@ require 'spec_helper'
 require 'pratt'
 
 describe Customer do
+  it_should_behave_like "being a billable item"
+
   describe "instance" do
     before :each do
-      @customer = Customer.new
+      @customer = Customer.create :name => "frogstarr78", :address => '312 NW 7th', :zip => '97862'
+    end
+
+    after :each do
+      Customer.find_by_name('frogstarr78').destroy
     end
 
     it "responds to :projects" do
-      @customer.should respond_to(:projects)
+      Customer.new.should respond_to(:projects)
     end
 
     it "responds to :payment" do
-      @customer.should respond_to(:payment)
+      Customer.new.should respond_to(:payment)
+    end
+
+    it "calculates amount correctly" do
+      payment = mock('payment')
+      payment.expects(:rate).once.returns(31500)
+      @customer.expects(:payment).once.returns(payment)
+      @customer.amount.should == 315.0
     end
   end
 end
