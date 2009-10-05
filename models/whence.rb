a17ce19b2229fa3f893ba86a7276d474511fe4cf
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-04 18:05:55 -0700

    Removed comments. Fixed inspect when start/end_at is nil

--- models/whence.rb
+++ models/whence.rb
@@ -27,26 +27,14 @@ class Whence < ActiveRecord::Base
   end
 
   def inspect
-    format = ""
-    "#<Whence id: '#{id}', project: '#{project.name}', start_at: '#{start_at.strftime(Pratt::FMT)}', end_at: '#{end_at.strftime(Pratt::FMT)}'>"
+    "#<Whence id: '#{id}', project: '#{project.name}', start_at: '#{start_at? ? start_at.strftime(Pratt::FMT) : ''}', end_at: '#{end_at? ? end_at.strftime(Pratt::FMT) : ''}'>"
   end
   
   class << self
     include Pratt::Models
-#    def conditions_for_time_spent scale = nil, when_to = Time.now, *rest
-#      when_to = Chronic.parse(when_to) if when_to.is_a?(String)
-#      cond = ["end_at IS NOT NULL"]
-#      cond = [(cond << "start_at BETWEEN ? AND ?").join(' AND ')] | [when_to.send("beginning_of_#{scale}"), when_to.send("end_of_#{scale}")] unless scale.nil?
-#      cond << rest
-#    end
+
     def time_spent scale = nil, when_to = Time.now
       spent(self).call(scale, when_to)
-#      Proc.new {|*scale_when|
-#        scale, when_to, rest = scale_when
-#        what.send(:all, :conditions => conditions_for_time_spent(scale, when_to, rest), :include => :project ).inject(0.0) {|total, whence| 
-#          total += ( whence.end_at - whence.start_at )
-#        } / 3600
-#      }
     end
 
     def last_unended
