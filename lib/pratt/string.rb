43b1d6694a29994512e38c13bac9eca96d5ba0eb
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-15 09:56:05 -0700

    Correctly patched Colored to not color when expected

--- lib/pratt/string.rb
+++ lib/pratt/string.rb
@@ -1,26 +1,18 @@
 class Pratt
   module ColorString
-
-    COLORS = { 
-      'black'   => 30,
-      'red'     => 31, 
-      'green'   => 32, 
-      'yellow'  => 33,
-      'blue'    => 34,
-      'magenta' => 35,
-      'cyan'    => 36,
-      'white'   => 37
-    }.each do |color, key|
-      define_method color do
-        return self unless Pratt.color?
-        "\e[#{key}m#{self}#{clear}"
-      end
+    def or format_alternative
+      return self unless Pratt.color?
+      format_alternative
     end
+  end
 
-    def clear
-      "\e[0m"
+  module Colored
+    def colorize(string, options = {})
+      return self unless Pratt.color?
+      super 
     end
   end
 end
 
 String.send :include, Pratt::ColorString
+String.send :include, Pratt::Colored
