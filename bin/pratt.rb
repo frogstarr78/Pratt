91d19c7c10351f9acb649943d5d152411628181c
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-18 04:17:38 -0700

    I was being too optimistic w/ my use of Dir.pwd oh well.

--- bin/pratt.rb
+++ bin/pratt.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
-FileUtils.chdir Dir.pwd
+bin_file = __FILE__
+bin_file = File.symlink?(bin_file) ? File.readlink(bin_file) : bin_file
+
+bin_path = File.expand_path( '..', bin_file )
+pratt_dir = File.dirname( bin_path )
+FileUtils.chdir pratt_dir
 
 require "lib/pratt"
 
