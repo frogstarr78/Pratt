8558298830936280c0219c35b10fa3b8071c5c84
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-18 00:26:07 -0700

    Adding some helper methods

--- config.rb
+++ config.rb
@@ -2,15 +2,24 @@ require 'active_record'
 PRATT_ENV = ENV["PRATT_ENV"] || 'development' unless Object.const_defined? :PRATT_ENV
 
 class Pratt
+  @@connected_to = nil
   module Test
   end
 
   include FileUtils
 
   class << self
-    def connect to_env = :development
+    def connected_to?
+      @@connected_to
+    end
+
+    def connect! to_env = :development
       puts "Connecting to: #{to_env}"
-      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database  => "#{to_env}.sqlite3" )
+      @@connected_to = "#{to_env}.sqlite3"
+      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database => connected_to? )
+      unless File.exists? connected_to?
+        Pratt.migrate
+      end
     end
 
     def connected?
