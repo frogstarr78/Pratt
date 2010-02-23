6e82f45e6757080c8d1f9a1fe3778cbdff8b4688
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-02-23 03:36:14 -0800

    Adding money class

--- lib/pratt/float.rb
+++ lib/pratt/float.rb
@@ -1,5 +1,22 @@
+def Money float
+  Money.new(float)
+end
+
 class Float
-	def pretty_print
-		"%0.2f"% self
-	end
+  def pretty_print
+    "%0.2f"% self
+  end
+
+  def to_money
+    Money(self)
+  end
+end
+
+class Money
+  def initialize f
+    @f = Float(f)
+  end
+  def pretty_print
+    "$#{@f.pretty_print}"
+  end
 end
