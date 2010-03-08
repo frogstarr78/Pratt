4d4526398022c50247d4b2dcaffb337513ac4d13
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-03-08 02:33:24 -0800

    Added documentation

--- lib/pratt/array.rb
+++ lib/pratt/array.rb
@@ -1,6 +1,11 @@
 class Pratt
   class Formats
     module Array
+      ##
+      # Turns an array into a sentence
+      #
+      # @param [String] conjunction
+      # @return [String] 
       def to_sentence conjunction = 'and'
         self[0..-2].join(", ") << (self.size > 2 ? ',' : '') << " #{conjunction} #{self.last}"
       end
