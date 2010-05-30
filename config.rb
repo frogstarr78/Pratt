cb5d96c4711f87b52eea0fde0b42d0b587501d7b
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-05-29 23:27:30 -0700

    Removed extra and unnecessary stuff

--- config.rb
+++ config.rb
@@ -2,22 +2,13 @@ require 'active_record'
 PRATT_ENV = ENV["PRATT_ENV"] || 'development' unless Object.const_defined? :PRATT_ENV
 
 class Pratt
-  @@connected_to = nil
-  module Test
-  end
-
   include FileUtils
 
   class << self
-    def connected_to?
-      @@connected_to
-    end
-
     def connect! to_env = :development
-      puts "<!-- Connecting to: #{to_env} -->"
-      @@connected_to = "db/#{to_env}.sqlite3"
-      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database => connected_to? )
-      unless File.exists? connected_to?
+      connect_to = "db/#{to_env}.sqlite3"
+      ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database => connect_to  )
+      unless File.exists? connect_to
         Pratt.migrate
       end
     end
@@ -25,10 +16,5 @@ class Pratt
     def connected?
       ActiveRecord::Base.connected?
     end
-    
-    def config= env
-      include Pratt::Test
-    end
   end
-
 end
