625e46d35232b07b99b1b6eeef78defcaea83808
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-05-31 00:48:43 -0700

    Renamed test method

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'pratt'
 
 describe Money do
-  it "pretty_print generates formatted string" do
+  it "generates expected pretty_print output" do
     @money = Money.new 2.5
     @money.pretty_print.should eql("$2.50")
   end
